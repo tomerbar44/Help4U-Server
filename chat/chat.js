daeda693cf8ba30dc8052062ac58e692527c251c
tomerbar44 <tomebar44@gmail.com>
2020-02-24 00:45:54 +0200

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -1,35 +1,32 @@
 module.exports = function (http) {
     const io = require('socket.io')(http);
     const { updateChatFromSocket } = require('./updateChat');
-    
+
     // chat logic
     io.on('connection', function (socket) {
         socket.on('disconnect', function () {
-            // update chat in our db
-            updateChatFromSocket(socket.taskID, socket.chat);
-    
         });
-        socket.on('chat message', function (msg, from) {
+
+        /**
+         * added currTaskID,currTaskChat
+         */
+        socket.on('chat message', function (msg, from, currTaskID, currTaskChat) {
+            updateChatFromSocket(currTaskID, [...currTaskChat, { from: from, message: msg }]);
             io.emit('chat message', msg, from);
-    
         });
-    
         // we update the socket with task info
         socket.on("update task", function (task) {
-            socket.taskID = task.taskID;    
+            socket.taskID = task.taskID;
         })
-    
         // every message sent, we update the chat property of the socket, when user disconnect we will post the new chat 
         socket.on('update chat', function (chat) {
+            console.log('"updated chat" chat\n', chat)
             socket.chat = chat;
-    
+
+
         })
     });
-    
+
     return io;
 
 }
-
-
-
-
