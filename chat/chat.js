7f0ce842da0d935b8976367e93514f2454fe9c41
tomerbar44 <tomebar44@gmail.com>
2020-02-25 22:44:42 +0200

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -8,7 +8,6 @@ module.exports = function (http) {
         // every message arrived we updated the db and sending the new message to all clients socket 
         socket.on('chat message', function (msg, from, currTaskID, currTaskChat) {
             try{
-                io.emit('DB not updated chat', msg,from, "e.message");
                 updateChatFromSocket(currTaskID, [...currTaskChat, { from: from, message: msg }]);
                 io.emit('chat message', msg, from);
             }catch(e){
